c6ce3ef00452f654fbab2ed60bde6df9e590375a
donghyounkang <donghyounkang@gmail.com>
2018-02-28 17:14:35 +0900

    update

--- bitcamp-java-basic/src/step01/Exam04.java
+++ bitcamp-java-basic/src/step01/Exam04.java
@@ -3,10 +3,31 @@ package step01;
 public class Exam04{
     public static void main(String[] args) {
         //숫자: 그대로 적기
+        //정수
+
+        //=> 4byte크기의 정수
         System.out.println(10);
         System.out.println(+10);
         System.out.println(-10);
+
+        //=> 8byte크기의 정수
+        //   숫자 뒤에 L,l을 붙인다
+        System.out.println(10L);
+        System.out.println(+10l);
+        System.out.println(-10L);
+
+        //부동소수점
+
+        //=> 4byte크기
+        //   숫자 뒤에 F,f를 붙인다
+        System.out.println(3.14f);
+        System.out.println(3.14F);
+
+        //=>8byte크기의 정수
         System.out.println(3.14);
+        System.out.println(31.4e-1);
+
+        
 
         //문자열: ""
         System.out.println("오호라");
@@ -20,5 +41,5 @@ public class Exam04{
     }
 }
 
-//자바코드로 값을 표현(literal)하는 것
+//자바코드로 값을 표현(literal)
 
