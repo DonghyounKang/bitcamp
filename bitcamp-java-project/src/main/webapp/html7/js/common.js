d556f5da0f68d04b6034ff56ab3ba36e629beee4
donghyounkang <donghyounkang@gmail.com>
2018-08-16 16:02:02 +0900

    asdf

--- bitcamp-java-project/src/main/webapp/html7/js/common.js
+++ bitcamp-java-project/src/main/webapp/html7/js/common.js
@@ -1,3 +1,3 @@
 /*var serverRoot = "http://localhost:8888/bitcamp-java-project";*/
 /*var serverRoot = "http://www.donghyounkang.xyz:8888/bitcamp-java-project";*/
-var serverRoot = "http://13.125.208.59:8888/bitcamp-java-project";
+var serverRoot = "http://13.125.208.59:8888/bitcamp-java-project"; 
